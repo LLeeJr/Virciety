fce1818297d26ad70e6b95746e0927fb865dd1e0
fabian.muehlenbein <fabian15121998@googlemail.com>
2021-07-11 16:22:37 +0200

    refactor: remove gateway-service sub-project

--- server/gateway-service/gateway/service.go
+++
@@ -1,8 +0,0 @@
-package gateway
-
-import "context"
-
-type Service interface {
-	CreateDMEvent(ctx context.Context, id string, msg string) (string, error)
-	GetDMEvent(ctx context.Context, id string) (string, error)
-}
--- server/gateway-service/go.mod
+++
@@ -1,8 +0,0 @@
-module gateway-service
-
-go 1.16
-
-require (
-	github.com/go-kit/kit v0.10.0
-	github.com/lib/pq v1.10.2
-)
