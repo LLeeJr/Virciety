26e9c41682148f85483bd76bd7d28c4e63a41302
fabian.muehlenbein <fabian15121998@googlemail.com>
2022-01-12 11:38:07 +0100

    feat: restyle chat entirely

--- client/projects/chat/src/app/material/material.module.ts
+++ client/projects/chat/src/app/material/material.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import {MatListModule} from "@angular/material/list";
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -8,10 +11,16 @@ import {MatCardModule} from "@angular/material/card";
   imports: [
     MatListModule,
     MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     MatListModule,
     MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
   ],
 })
 export class MaterialModule { }
