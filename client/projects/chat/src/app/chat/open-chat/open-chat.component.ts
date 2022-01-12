54de98e50e495fa35e352d24fe4a5bf765be3277
fabian.muehlenbein <fabian15121998@googlemail.com>
2022-01-12 14:15:50 +0100

    fix: fix chat automatic scroll down + retrieve room name from route params

--- client/projects/chat/src/app/chat/open-chat/open-chat.component.ts
+++ client/projects/chat/src/app/chat/open-chat/open-chat.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from "../../api/api.service";
 import {Dm} from "../../data/dm";
 import {KeycloakService} from "keycloak-angular";
 import {Subscription} from "rxjs";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-open-chat',
@@ -11,7 +12,6 @@ import {Subscription} from "rxjs";
 })
 export class OpenChatComponent implements OnInit, OnDestroy {
 
-  user1: string | null = '';
   messages: Dm[] = [];
 
   message: string = '';
@@ -19,12 +19,15 @@ export class OpenChatComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
 
   constructor(public api: ApiService,
-              private keycloak: KeycloakService) { }
+              private elem: ElementRef,
+              private keycloak: KeycloakService,
+              private route: ActivatedRoute) { }
 
   ngOnDestroy(): void {
     this.messages = [];
     this.subscription.unsubscribe();
     this.api.unsubscribeToChat();
+    this.api.selectedRoom = null;
   }
 
   ngOnInit(): void {
@@ -34,30 +37,39 @@ export class OpenChatComponent implements OnInit, OnDestroy {
       }
     });
 
-    this.subscription = this.api.getMessagesFromRoom().subscribe(value => {
-      this.messages = value.data.getMessagesFromRoom;
+    this.route.params.subscribe((params: Params) => {
+      let roomName = params['name'];
+      this.subscription = this.api.getMessagesFromRoom(roomName).subscribe(value => {
+        this.messages = value.data.getMessagesFromRoom;
+      });
+
+      this.api.subscribeToChat(roomName).subscribe(value => {
+        if (!value || !value.data || !value.data.dmAdded) {
+          return;
+        }
+
+        const { data } = value
+
+        const { chatroomId, createdAt, createdBy, msg, __typename } = data.dmAdded;
+
+        const dm = new Dm(chatroomId, createdAt, createdBy, msg, __typename);
+        this.messages = Object.assign([], this.messages)
+        this.messages.push(dm);
+      });
     });
-
-    this.api.subscribeToChat().subscribe(value => {
-      if (!value || !value.data || !value.data.dmAdded) {
-        return;
-      }
-
-      const { data } = value
-
-      const { chatroomId, createdAt, createdBy, msg, __typename } = data.dmAdded;
-
-      const dm = new Dm(chatroomId, createdAt, createdBy, msg, __typename);
-      this.messages = Object.assign([], this.messages)
-      this.messages.push(dm);
-    });
-
   }
 
   async sendMessage() {
     if (this.message.length > 0) {
       await this.api.writeDm(this.message).toPromise();
       this.message = '';
+      this.scrollToBottom();
     }
+  }
+
+  private scrollToBottom() {
+    try {
+      this.elem.nativeElement.scrollTop = this.elem.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 }
